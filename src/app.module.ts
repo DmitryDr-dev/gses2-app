32416a5483768799663ae4e8f16e54e3b94bc197
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-30 15:52:55 +0300

    feat: add exchange api module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ExchangeApiModule } from './exchange-api/exchange-api.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    ExchangeApiModule,
   ],
   controllers: [],
   providers: [],
