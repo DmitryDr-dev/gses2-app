aa7540bfcb9e56fb5f0b562f2a6077660703b6cf
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-30 15:22:39 +0300

    feat: add config module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.APP_PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('gses2.app/api');
+  app.enableCors();
+
+  await app.listen(PORT, () => console.info(`Server runs on port: ${PORT}`));
 }
 bootstrap();
