246b311660f2c975675972d3e96e8ab8ff7f13e8
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-30 16:47:25 +0300

    feat: add db bst service to work with local data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ExchangeApiModule } from './exchange-api/exchange-api.module';
 import { RateModule } from './rate/rate.module';
+import { DbBinaryTreeModule } from './database/db-binary-tree/db-binary-tree.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { RateModule } from './rate/rate.module';
     }),
     ExchangeApiModule,
     RateModule,
+    DbBinaryTreeModule,
   ],
   controllers: [],
   providers: [],
