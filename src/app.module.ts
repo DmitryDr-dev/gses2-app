7d6b89824dfdd8b4a2c18e08a01f3de807f5f1a5
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-07-31 18:50:14 +0300

    feat: add bull module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ExchangeApiModule } from './exchange-api/exchange-api.module';
 import { RateModule } from './rate/rate.module';
 import { DbBinaryTreeModule } from './database/db-binary-tree/db-binary-tree.module';
@@ -7,11 +7,22 @@ import { DbStorageModule } from './database/db-storage/db-storage.module';
 import { LocalDbModule } from './database/local-db/local-db.module';
 import { MailModule } from './mail/mail.module';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        redis: {
+          host: config.get('REDIS_HOST'),
+          port: config.get('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
     }),
     ExchangeApiModule,
     RateModule,
