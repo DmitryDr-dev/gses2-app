654f4f2c4ee2e181a3d6f52b1b14181a92b22f4a
DmitryDr-dev <79312440+DmitryDr-dev@users.noreply.github.com>
2022-08-29 17:40:25 +0300

    refactor: refactor subscription module

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { SubscriptionService } from './subscription.service';
-import { SubscriptionController } from './subscription.controller';
 import { LocalDbModule } from 'src/database/local-db/local-db.module';
-import { MailModule } from 'src/mail/mail.module';
 import { ExchangeApiModule } from 'src/exchange-api/exchange-api.module';
+import { MailModule } from 'src/mail/mail.module';
+import { SubscriptionController } from './subscription.controller';
+import { SubscriptionService } from './subscription.service';
 
 @Module({
   imports: [LocalDbModule, MailModule, ExchangeApiModule],
